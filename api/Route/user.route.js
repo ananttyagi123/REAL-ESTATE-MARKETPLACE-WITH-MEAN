0b90c96490145cbc921d2915609ec73053f746ea
Ananttyagi123 <ananttyagi359@gmail.com>
2024-02-23 23:08:06 +0530

    Update the route

--- api/Route/user.route.js
+++ api/Route/user.route.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import {test} from '../UserController/user.controller.js';
+import { test } from '../UserController/user.controller.js';
+import { verifyToken } from '../utils/VerifyUser.js';
+import updateUser from '../UserController/user.controller.js'
 const router = express.Router();
 
 router.get('/test', test)
 
-
+// create an API for updating the User Profile Data
+router.post('/update/:id',verifyToken, updateUser);
 export default router;
+
+
